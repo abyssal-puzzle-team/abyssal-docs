1fc79f074cf88057345abd15f0612932aede9f35
HaiHome <HaiHome@yeah.net>
2026-02-02 10:04:54 +0800

    修改主页、svg、侧边栏、文档页面

---
+++ docs/abyssal-wechat.md
@@ -0,0 +1,22 @@
+---
+title: Abyssal WeChat
+description: Abyssal WeChat 相关文档
+---
+
+# Abyssal WeChat
+
+欢迎使用 Abyssal WeChat 文档。
+
+## 概述
+
+这里将包含 Abyssal WeChat 项目的相关文档内容。
+
+## 功能特性
+
+- 功能一
+- 功能二
+- 功能三
+
+## 快速开始
+
+详细的安装和使用指南即将推出。
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'abyssal-puzzle-team', // Usually your GitHub org/user name.
+  projectName: 'abyssal-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -50,22 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+            'https://github.com/abyssal-puzzle-team/abyssal-docs',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -101,42 +86,25 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Information',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Website',
+                href: 'https://abyssal-puzzle.top',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'GitHub',
+                href: 'https://github.com/abyssal-puzzle-team',
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: 'Blog',
+                to: '/blog',
               },
             ],
           },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
             ],
           },
         ],
